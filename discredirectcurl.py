23c8c42ad69809ee73ba1f768f5a323fd9bbd5f6
stnrik <vetlelb@matnat.uio.no>
2019-11-24 11:33:39 +0100

    Cleanup

--- discredirectcurl.py
+++ discredirectcurl.py
@@ -1,17 +1,19 @@
 # -*- coding: UTF-8 -*-
 
-import json, re, hexchat,requests
-#from io import BytesIO
+import json
+import re
+import hexchat
+import requests
 
 __module_name__ = "hextodiscurl"
 __module_version__ = "1.0"
 __module_description__ = "Redirect HexChat->Discord"
 
-URL = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+URL = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 HOOKURL = "put API key here"
 
 
-def forward_cb(word,word_eol,userdata):
+def forward_cb(word, word_eol, userdata):
     nick = word[0]
     message = word[1]
 
@@ -19,14 +21,15 @@ def forward_cb(word,word_eol,userdata):
     message = message.split()
     messages = ""
     for text in message:
-        if re.match(URL,text):
+        if re.match(URL, text):
             messages = (messages + ("``" + text + "``" + " "))
         else:
             messages += (text + " ")
 
-    payload = json.dumps({"username":nick,"content":messages})
-	requests.post(URL,headers = ['Content-type:application/json'],data=payload)
+    payload = json.dumps({"username": nick, "content": messages})
+    requests.post(HOOKURL, headers={'Content-type':'application/json'}, data=payload)
     return hexchat.EAT_NONE
+
 
 hexchat.hook_print("Channel Message", forward_cb)
 hexchat.hook_print("Your Message", forward_cb)
